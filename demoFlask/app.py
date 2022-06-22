9a707aa7475bd5b9e2c1e761af615e08c74c3ce1
dabinghehehe <956574869@qq.com>
2022-06-22 21:29:55 +0800

    first commit

---
+++ demoFlask/app.py
@@ -0,0 +1,14 @@
+import webbrowser
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__, static_url_path='/',
+            static_folder='static', template_folder='templates')
+
+
+@ app.route("/")
+def direct_show():
+    return render_template("index-1.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
